0c01539baa0267a2d0c603ebd6510c76d59fa5b6
Rocimar Perez <58374420+rossyalex@users.noreply.github.com>
2022-10-30 21:21:13 -0300

    bases de las rutas

---
+++ src/components/navigation/Navbar.jsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+
+export function Navbar() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Login</Link>
+          </li>
+          <li>
+            <Link to="home">Home</Link>
+          </li>
+          <li>
+            <Link to="order">Order</Link>
+          </li>
+          <li>
+            <Link to="kitchen">Kitchen</Link>
+          </li>
+          <li>
+            <Link to="withdraw">Withdraw</Link>
+          </li>
+        </ul>
+      </nav>
+      <nav>
+
+
+
+      </nav>
+    </div>
+  )
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,21 @@
+export const Login = () => {
+  return(
+    <div>
+      Login
+      <p>
+        User
+      </p>
+      <p>
+        Password
+      </p>
+    </div>
+  )
+}
+
+export const Home = () => <h3>Home Burger</h3>
+
+export const Order = () => <h3>Order Burger</h3>
+
+export const Kitchen = () => <h3>Kitchen Burger</h3>
+
+export const Withdraw = () => <h3>Withdraw Burger</h3>
